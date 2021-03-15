9a84716096e902a9090aaddddef6efe5e22658c6
Internet Music <thor@internetmusic.ltd.uk>
2021-03-15 05:07:14 +0100

    Com 1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ class Nav extends Component {
           <span
             className="navbar-brand col-sm-3 col-md-2 mr-0"
           >
-            NFT Gallery
+            Copyright Flow NFT
           </span>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
